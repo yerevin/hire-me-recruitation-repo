fc87cc1d7cd15614fcca85b217f834f957e418df
yerevin <skulskimat@gmail.com>
2020-07-21 22:12:16 +0200

    [Angular] Dead code in e2e tests cleaning

--- angular-rxjs/e2e/src/app.e2e-spec.ts
+++ angular-rxjs/e2e/src/app.e2e-spec.ts
@@ -14,7 +14,6 @@ describe('app', () => {
     page.fillLoginForm();
     page.submitLoginForm();
     expect(page.getTasksListTitle()).toEqual('Tasks');
-    // browser.sleep(5000);
   });
 
   it('should open task add form and add task then show tasks list page', async () => {
@@ -22,53 +21,11 @@ describe('app', () => {
     page.fillTaskAddForm();
     page.submitTaskAddForm();
     expect(page.getTasksListTitle()).toEqual('Tasks');
-    // browser.sleep(5000);
   });
 
   it("should find added task - 'Test' on list", async () => {
     expect(page.getTaskOnListName()).toEqual(DATA.addedTaskName);
-    // browser.sleep(5000);
   });
-
-  // it('should match a form with a "loginForm" name then fill its controls', async () => {
-  //   page.fillLoginForm()
-  //   await expect(page).toFillForm('form[name="loginForm"]', {
-  //     email: 'test@test.com',
-  //     password: 'testtest',
-  //   });
-  // });
-
-  // it('should find login submit button and click it to process login', async () => {
-  //   await expect(page).toClick('#loginBtn');
-  //   await page.waitFor(1000);
-  // });
-
-  // it('should display Tasks page', async () => {
-  //   await expect(page).toMatch('Tasks');
-  // });
-
-  // it('should find add task button and click it to open add task form', async () => {
-  //   await expect(page).toClick('#addTaskBtn');
-  // });
-
-  // it('should match a form with a "taskForm" name then fill its controls', async () => {
-  //   await expect(page).toFillForm('form[name="taskForm"]', {
-  //     name: 'Test',
-  //   });
-  // });
-
-  // it('should find submit button and click it to add task', async () => {
-  //   await expect(page).toClick('#taskSaveBtn');
-  //   await page.waitFor(1000);
-  // });
-
-  // it('should display Tasks page', async () => {
-  //   await expect(page).toMatch('Tasks');
-  // });
-
-  // it("should find added task - 'Test' on list", async () => {
-  //   await expect(page).toMatch('Test');
-  // });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
